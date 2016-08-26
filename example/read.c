cf8864bdce1007d1676e7cef841f5d4ec256fc58
substack <substack@9nTgtYmvW4HID6ayt6Icwc8WZxdifx5SlSKKIX/X/1g=.ed25519>
2016-08-25 20:57:22 -0700

    sort of works

--- example/read.c
+++ example/read.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <o5mdecoder.h>
 
 int main (int argc, char **argv) {
   char *data = (char*) malloc(4096);
-  o5mdecoder::Decoder d;
-  o5mdecoder::Doc *doc;
+  char *dbuf = (char*) malloc(4096);
+  o5mdecoder::Decoder d(dbuf);
+  o5mdecoder::Doc doc;
   size_t len;
   do {
     len = fread(data, sizeof(char), 4096, stdin);
     d.write(data, len);
-    while ((doc = d.read()) !== NULL) {
-      printf("%d\n", doc.id);
+    try {
+      while (d.read(&doc)) {
+        printf("id=%d\n", doc.id);
+      }
+    } catch (char *err) {
+      fprintf(stderr, "error: %s\n", err);
     }
   } while (len == 4096);
   return 0;
--- o5mdecoder.h
+++ o5mdecoder.h
@@ -2,6 +2,9 @@
 #define O5MDECODER_H
 
 #include <stdint.h>
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
 
 namespace o5mdecoder {
   typedef unsigned char TYPE;
@@ -9,11 +12,12 @@ namespace o5mdecoder {
   const TYPE WAY = 0x11;
   const TYPE REL = 0x12;
 
-  typedef unsigned char _STATE;
   enum _STATE {
-    _BEGIN = 1;
-    _START = 2;
-    _LEN = 3;
+    _BEGIN = 1,
+    _TYPE = 2,
+    _LEN = 3,
+    _DATA = 4,
+    _END = 5
   };
 
   class Doc {
@@ -44,14 +48,68 @@ namespace o5mdecoder {
   };
   class Decoder {
     public:
-    char *buffer;
-    size_t length;
-    size_t pos;
-    Decoder (char *data, size_t len) {
+    char *buffer, *docbuf;
+    size_t length, pos;
+    size_t doclen, docpow, docsize;
+    char _err[256];
+
+    _STATE _state;
+    Decoder (char *dbuf) {
+      _state = _BEGIN;
+      buffer = NULL;
+      pos = 0;
+      docbuf = dbuf;
+      length = 0;
+      docpow = 1;
+      doclen = 0;
+      docsize = 0;
+    }
+    void write (char *data, size_t len) {
       buffer = data;
       length = len;
     }
-    Doc *read () {
+    bool read (Doc *doc) {
+      size_t j;
+      for (; pos < length; pos++) {
+        unsigned char b = buffer[pos];
+        if (_state == _BEGIN && b != 0xff) {
+          sprintf(_err, "first byte in frame, expected 0xff, got 0x%x", b);
+          throw _err;
+        } else if (_state == _BEGIN) {
+          _state = _TYPE;
+        } else if (_state == _TYPE && b == 0xff) { // reset
+          _state = _TYPE;
+        } else if (_state == _TYPE) {
+          _state = _LEN;
+          doc->type = b;
+          printf("type=0x%x\n", doc->type);
+        } else if (_state == _LEN) {
+          doclen += (b & 0x7f) * docpow;
+          printf("doclen=%u, docpow=%u, b=%u\n", doclen, docpow, b);
+          docpow *= 0x80;
+          if (b < 0x80) {
+            _state = _DATA;
+          }
+        } else if (_state == _DATA) {
+          j = fminl(length, pos + doclen - docsize);
+          memcpy(docbuf+docsize, buffer+pos, j-pos);
+          docsize += j - pos;
+          pos += j - 1;
+          printf("cmp %u, %u; j=%u\n", docsize, doclen, j);
+          if (docsize == doclen) {
+            printf("doc %d bytes\n", docsize);
+            // parse docbuf...
+            _state = _TYPE;
+            doclen = 0;
+            docpow = 1;
+            docsize = 0;
+            //return true;
+          }
+        } else if (_state == _END) {
+          // ...
+        }
+      }
+      return false;
     }
   };
 }
