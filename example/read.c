c5ddc6e0c6d54936299ba2afc073242c9de6b052
substack <substack@9nTgtYmvW4HID6ayt6Icwc8WZxdifx5SlSKKIX/X/1g=.ed25519>
2016-09-13 00:46:47 -0400

    number parsing, polymorphic switching, nodes fleshed out

--- example/read.c
+++ example/read.c
@@ -6,14 +6,23 @@ int main (int argc, char **argv) {
   char *data = (char*) malloc(4096);
   char *dbuf = (char*) malloc(4096);
   o5mdecoder::Decoder d(dbuf);
-  o5mdecoder::Doc doc;
+  o5mdecoder::Node node;
+  o5mdecoder::Way way;
+  o5mdecoder::Rel rel;
   size_t len;
   do {
     len = fread(data, sizeof(char), 4096, stdin);
     d.write(data, len);
     try {
-      while (d.read(&doc)) {
-        printf("id=%d\n", doc.id);
+      while (d.read(&node, &way, &rel)) {
+        if (d.type == o5mdecoder::NODE) {
+          printf("<node id=\"%d\" lon=\"%f\" lat=\"%f\"/>\n",
+            node.id, node.lon, node.lat);
+        } else if (d.type == o5mdecoder::WAY) {
+          printf("<way id=\"%d\"/>\n", way.id);
+        } else if (d.type == o5mdecoder::REL) {
+          printf("<relation id=\"%d\"/>\n", rel.id);
+        }
       }
     } catch (char *err) {
       fprintf(stderr, "error: %s\n", err);
--- o5mdecoder.h
+++ o5mdecoder.h
@@ -20,18 +20,17 @@ namespace o5mdecoder {
     _END = 5
   };
   size_t signedDelta (int64_t *d, char *data, size_t len) {
-    unsigned char m = 0x3f;
     size_t i;
-    int64_t npow = 1 - 2*((data[0]>>6)&1);
+    unsigned char b, m = 0x40;
+    int64_t npow = data[0]%2 ? -1 : 1;
     for (i = 0; i < len; i++) {
-      unsigned char b = data[i];
-      *d += (b & m) * npow;
-      npow *= m;
+      b = data[i] >> (i == 0 ? 1 : 0);
+      *d += (b % m) * npow;
       if (b < 0x80) break;
-      m = 0x7f;
+      npow *= m;
+      m = 0x80;
     }
-    (*d)--;
-    return i;
+    return i+1;
   }
   size_t signedDelta (uint64_t *d, char *data, size_t len) {
     return signedDelta((int64_t*)d, data, len);
@@ -51,7 +50,7 @@ namespace o5mdecoder {
     size_t _taglen;
     char *_tags;
     Doc () {
-      id = 2;
+      id = 0;
       version = 0;
       timestamp = 0;
       changeset = 0;
@@ -65,6 +64,14 @@ namespace o5mdecoder {
     public:
     double lat;
     double lon;
+    int64_t ulat;
+    int64_t ulon;
+    Node () {
+      lon = 0;
+      lat = 0;
+      ulon = 0;
+      ulat = 0;
+    }
   };
   class Way : public Doc {
     public:
@@ -78,6 +85,11 @@ namespace o5mdecoder {
     size_t length, pos;
     size_t doclen, docpow, docsize;
     char _err[256];
+    Node _prevNode;
+    Way _prevWay;
+    Rel _prevRel;
+    Doc *_prevDoc;
+    TYPE type;
 
     _STATE _state;
     Decoder (char *dbuf) {
@@ -89,12 +101,13 @@ namespace o5mdecoder {
       docpow = 1;
       doclen = 0;
       docsize = 0;
+      _prevDoc = NULL;
     }
     void write (char *data, size_t len) {
       buffer = data;
       length = len;
     }
-    bool read (Doc *doc) {
+    bool read (Node *node, Way *way, Rel *rel) {
       size_t j;
       for (; pos < length; pos++) {
         unsigned char b = buffer[pos];
@@ -107,7 +120,7 @@ namespace o5mdecoder {
           _state = _TYPE;
         } else if (_state == _TYPE) {
           _state = _LEN;
-          doc->type = b;
+          type = b;
         } else if (_state == _LEN) {
           doclen += (b & 0x7f) * docpow;
           docpow *= 0x80;
@@ -120,7 +133,9 @@ namespace o5mdecoder {
           docsize += j - pos;
           pos = j;
           if (docsize == doclen) {
-            _parseDoc(doc, doclen, docbuf);
+            if (type == NODE) _parseNode(node, doclen, docbuf);
+            else if (type == WAY) _parseWay(way, doclen, docbuf);
+            else if (type == REL) _parseRel(rel, doclen, docbuf);
             _state = _TYPE;
             doclen = 0;
             docpow = 1;
@@ -133,9 +148,39 @@ namespace o5mdecoder {
       }
       return false;
     }
-    void _parseDoc (Doc *doc, size_t len, char *buf) {
+    size_t _parseDoc (Doc *doc, size_t len, char *buf) {
       size_t pos = 0;
-      pos += signedDelta(&(doc->id), buf+pos, len-pos);
+      if (_prevDoc) doc->id = _prevDoc->id;
+      pos += signedDelta(&(doc->id), buf+pos, len-pos); // id
+      if (buf[pos] == 0x00) { // version
+        pos++;
+      } else {
+        // ...
+      }
+      return pos;
+    }
+    void _parseNode (Node *node, size_t len, char *buf) {
+      size_t pos = _parseDoc(node, len, buf);
+      int64_t ulat = _prevNode.ulat;
+      int64_t ulon = _prevNode.ulon;
+      pos += signedDelta(&ulon, buf+pos, len-pos); // lon
+      pos += signedDelta(&ulat, buf+pos, len-pos); // lat
+      node->ulon = ulon;
+      node->ulat = ulat;
+      node->lon = ((double) ulon) / 1e7;
+      node->lat = ((double) ulat) / 1e7;
+      _prevNode = *node;
+      _prevDoc = node;
+    }
+    void _parseWay (Way *way, size_t len, char *buf) {
+      size_t pos = _parseDoc(way, len, buf);
+      _prevWay = *way;
+      _prevDoc = way;
+    }
+    void _parseRel (Rel *rel, size_t len, char *buf) {
+      size_t pos = _parseDoc(rel, len, buf);
+      _prevRel = *rel;
+      _prevDoc = rel;
     }
   };
 }
